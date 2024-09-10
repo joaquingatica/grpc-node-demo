bf5a07d0042a2c30d5d339513b55f6bb4c6532b8
Joaquin Gatica <joaquin@anagram.dev>
2024-09-10 01:55:30 -0300

    chore: cleanup and refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,21 @@
 import { config } from './config'
 import { server, setupServer } from './server'
 
-const serverShutdown = async () => {
-  await server.shutdown()
-  console.log('Server gracefully shut down.')
-}
-process.on('SIGINT', () => serverShutdown().then(() => process.exit(130)))
-process.on('SIGTERM', () => serverShutdown().then(() => process.exit(143)))
-
-const main = async () => {
+const start = async () => {
   await setupServer()
   const port = await server.listen(`0.0.0.0:${config.grpc.port}`)
   console.log(`🚀 Server is running in port ${port}`)
 }
 
-main().catch((error) => {
+const shutdown = async () => {
+  await server.shutdown()
+  console.log('Server gracefully shut down.')
+}
+
+start().catch((error) => {
   console.error(error)
   process.exit(1)
 })
+
+process.on('SIGINT', () => shutdown().then(() => process.exit(130)))
+process.on('SIGTERM', () => shutdown().then(() => process.exit(143)))
